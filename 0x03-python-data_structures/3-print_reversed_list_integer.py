a8aee6104d1a66421f07dc3cc5e357974935b645
JuneMbuvi <mbuvimbatha@gmail.com>
2023-08-14 10:14:37 -0700

    replacing in a copy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    new_list = [i for i in my_list]
-    new_list[idx] = element
-    return (new_list)
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
