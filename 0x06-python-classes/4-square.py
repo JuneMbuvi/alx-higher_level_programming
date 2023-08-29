2d435d4fea7649e7425b1f569d76672ad64e9f5e
JuneMbuvi <mbuvimbatha@gmail.com>
2023-08-29 11:14:21 -0700

    classes and public instances

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,14 +3,14 @@
 
 
 class Square:
+    """creating the private instance of the square"""
     def __init__(self, size=0):
-        """initializing the Square"""
-        self.__size = size
-        """using the getter to access the size property"""
+        self.size = size
+
     @property
     def size(self):
         return (self.__size)
-    """using the setter to update the class instances"""
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -18,6 +18,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    """creating the public instance area"""
+
     def area(self):
         return (self.__size * self.__size)
