642c525bbe25f35fe95fa3d780989e88a1353389
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-05 10:34:21 -0700

    adding integers

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""defines a function that prints a persons name.
+It takes both a first name and a second name.
+If a non-sring is passed, a TypeError is raised."""
+
+
+def say_my_name(first_name, last_name=""):
+    if not (isinstance(first_name, str)):
+        raise  TypeError("first_name must be a string")
+    if not(isinstnace(last_name, str)):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
