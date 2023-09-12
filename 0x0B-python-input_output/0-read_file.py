fb2e0e29b0fad1c26de7f48fae2cd7b849750766
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-12 06:25:11 -0700

    read files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a function for reading files"""
+
+
+def read_file(filename=""):
+    """we use the read() with the filename
+    we don't have to pass a mode since its default is read"""
+    with open("my_file_0.txt", encoding="utf-8") as f:
+        print(f.read())
