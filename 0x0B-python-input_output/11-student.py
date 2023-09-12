1efdeb59b5653c398fafb034e954f12afa0ddbcd
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-12 11:17:14 -0700

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines a student class"""
+
+
+class Student:
+    """constructor method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    """public method for retrieving dictionary representation of Student"""
+    def to_json(self, attrs=None):
+        """using type & getattr to test for type and get attributes"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        """the dict() is used to get dictionary representations"""
+        return (self.__dict__)
+
+    """creating an instance of deserialization"""
+    def reload_from_json(self, json):
+        for k, v in json.items():
+            setattr(self, k, v)
