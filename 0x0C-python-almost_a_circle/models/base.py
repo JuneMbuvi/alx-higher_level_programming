b5967b2bd573934d3e8632a598144ba6a3c1cb8c
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-15 11:27:55 +0000

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creates a class Base"""
+
+class Base:
+	"""create class atrribute which is private"""
+	__nb_objects = 0
+	"""constructor method"""
+	def __init__(self, id=None):
+		
