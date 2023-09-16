70ec1c1d5c16efdc69870c0f7c20759526fa42c0
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-16 08:51:13 -0700

    Rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -6,10 +6,13 @@ from models.rectangle import Rectangle
 
 class testRectangleClass(unittest.TestCase):
     """test the id of a rectangle"""
-    def test_rect_id(self):
+    def test_rect_instances(self):
         r1 = Rectangle(10, 2)
         self.assertIsInstance(Rectangle(10, 2), Base)
         r2 = Rectangle(2, 10)
         self.assertIsInstance(Rectangle(2, 10), Base)
-        r2 = Rectangle(2, 10)
+        r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertIsInstance(Rectangle(10, 2, 0, 0, 12), Base)
+
+if __name__ == "__main__":
+    unittest.main()
