555648d8d30061253c639ee97021aecc1ac724fc
JuneMbuvi <mbuvimbatha@gmail.com>
2023-09-18 23:10:25 -0700

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-import json
 """creates a class Base"""
+import json
 
 
 class Base:
-    """create class attribute(s)"""
+    """create class attribute(s) that increments everytime
+    an oobject is created"""
     __nb_objects = 0
     """constructor method"""
     def __init__(self, id=None):
+        """initializez the Base class with an id and returns id if found.
+        Args
+        id - returns id of the object created"""
         if id is not None:
             self.id = id
         else:
@@ -18,7 +22,10 @@ class Base:
     list_dictionaries"""
     @staticmethod
     def to_json_string(list_dictionaries):
-        """if list is empty return empty list"""
+        """Returns the string representation of list_dictionaries
+        if list is empty return empty list "[]".
+        Args
+        list_dictionaries - is a list of dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return (json.dumps(list_dictionaries))
