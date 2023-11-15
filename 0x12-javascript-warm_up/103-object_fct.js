54f7234beec7bd09c6fe945091829faa875d403e
JuneMbuvi <mbuvimbatha@gmail.com>
2023-11-15 04:44:57 -0800

    Incrementing Object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
 const myObject = {
   type: 'object',
-  value: 12,
-  incr () {
-    this.value++;
-  }
+  value: 12
 };
 console.log(myObject);
+function incr () {
+  this.value++;
+}
+myObject.incr = incr;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
