e2f1ba6968fa52597ca10d1830df47bcda14ae03
JuneMbuvi <mbuvimbatha@gmail.com>
2023-11-17 02:25:12 -0800

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""defines a script that lists all states with a name starting
+with N from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
-"""defines a script that lists all states with a name starting
-with N from the database hbtn_0e_0_usa"""
 
 if __name__ == '__main__':
     try:
