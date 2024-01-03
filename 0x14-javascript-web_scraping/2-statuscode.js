b6f518ccfc1401b0146d1a792cf17d6513377628
JuneMbuvi <mbuvimbatha@gmail.com>
2024-01-03 09:22:18 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,19 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const[url] = process.argv.slice(2);
-
-if (!url) {
-	console.error('Usage: node script.js <URL>');
-	process.exit(1);
-}
-
-//make a get request & check the status code
-request.get(url, (error, response) => {
-	if (error) {
-		console.error('Error:', error.message);
-	} else {
-		console.log(`code: ${response.statusCode}`);
-	}
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
