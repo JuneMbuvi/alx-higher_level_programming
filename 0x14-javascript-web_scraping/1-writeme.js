bbf918f3fbdf0917979e6b0ba1297c17762c9b59
JuneMbuvi <mbuvimbatha@gmail.com>
2024-01-03 09:15:11 -0800

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,17 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-const [filePath, content] = process.argv.slice(2);
-if (!filePath || !content) {
-  console.error('Usage: node script.js <file-path> <content>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error('Error writing to file:', err);
-  } else {
-    console.log(`Content has been written to ${filePath}`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
