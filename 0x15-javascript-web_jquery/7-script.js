e4e7e90ff0a3ca511b08fbc623a3dc29298746ab
JuneMbuvi <mbuvimbatha@gmail.com>
2024-01-10 08:10:08 -0800

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('#character').on('click', function () {
+    $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+      $('#character').text('Character Name: ' + data.name);
+    });
+  });
+});
